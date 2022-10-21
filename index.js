4a07f2f87d676a79a8f2f08092e8b3835d7a75c0
adhillon0817 <avleendhillon@gmail.com>
2022-10-20 18:13:56 -0700

    added notes outline

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-//View all departments 
+// View all departments 
+
+// SELECT * FROM department 
+
+/////////////////////////////////////////////////////
+
+// View all roles
+
+// SELECT * FROM role
+
+/////////////////////////////////////////////////////
+
+// View all employees 
+
+// SELECT * FROM employee;
+
+/////////////////////////////////////////////////////
+
+// Create a new department
+
+//Prompt the user for the "name" of the de
+partment
+
+        // THEN run the query
+        // INSERT INTO department (name)
+        // VALUES ("Sales");
+
+            // THEN ask the user what they want to do next
+
+/////////////////////////////////////////////////////
+
+//Create a new role
+
+
+
+// Get the existing departments from the 'departement' table
+
+    // THEN prompt the user for the "title", "salary", and "department" for the role
+
+            // THEN run the query
+            // INSERT INTO role (title, salary, department_id)
+            //  VALUES ("anyrole", 120000, 1)
+
+
+                    //THEN ask the user waht they want to do next
