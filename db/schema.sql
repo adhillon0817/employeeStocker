c1811fed6c19f71cd0b3fd630e7823fec87e1ebd
adhillon0817 <avleendhillon@gmail.com>
2022-11-13 23:33:55 -0800

    added all tables from project description

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,30 @@ CREATE DATABASE listofemployee_db;
 USE listofemployee_db;
 
 CREATE TABLE department (
-    id: INT PRIMARY KEY
+    id: INT NOT NULL,
     name VARCHAR (30),
     PRIMARY KEY (id)
+);
+CREATE TABLE role (
+    id INT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id)
+    REFERENCES department_id
+    ON DELETE SET NULL
+
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT REFERENCES employee(id),
+    FOREIGN KEY (role_id)
+    REFERENCES (id)
+    ON DELETE SET NULL
 );
