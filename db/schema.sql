bba70cd6505bedb42e2824645b793f641d6ccb90
adhillon0817 <avleendhillon@gmail.com>
2022-11-13 23:17:04 -0800

    added department table

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS listofemployee_db;
+CREATE DATABASE listofemployee_db;
+USE listofemployee_db;
+
+CREATE TABLE department (
+    id: INT PRIMARY KEY
+    name VARCHAR (30),
+    PRIMARY KEY (id)
+);
