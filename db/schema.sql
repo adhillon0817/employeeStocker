60e7e9d06798a7dbbf9ecd3dd91a219e7b0a7c94
adhillon0817 <avleendhillon@gmail.com>
2022-11-18 15:36:43 -0800

    changes in schema sql to match given tables on BCS

--- db/schema.sql
+++ db/schema.sql
@@ -3,26 +3,27 @@ CREATE DATABASE listofemployees_db;
 USE listofemployees_db;
 
 CREATE TABLE department (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR (30) NOT NULL
+    id INTEGER AUTO_INCREMENT NOT NULL,
+    name_department VARCHAR (30) NOT NULL,
+    PRIMARY KEY (id)
 );
-CREATE TABLE role (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INTEGER NOT NULL,
-    INDEX department_index(department_id),
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER,
-    INDEX role_index(role_id),
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-    manager_id INTEGER,
-    INDEX manager_index(manager_id),
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    role_id INT NOT NULL,
+    manager_id INT
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id),
+
 );
